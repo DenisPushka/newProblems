de698b981afe131f4a2ee3b72b2a46cbbe29a1a1
Denis <bardenbmw@gmail.com>
2021-10-29 00:22:09 +0400

    добавил аву

--- upload.js
+++ upload.js
@@ -1,11 +1,15 @@
 export function upload(selector, options = {}) {
   const input = document.getElementById('avatar')
   const customInput = document.getElementById('customInput')
+  const preview = document.createElement('div')
+
+  preview.classList.add('preview')
 
   if (options.accept) {
     input.setAttribute('accept', options.accept.join(','))
   }
 
+  input.insertAdjacentElement('afterend', preview)
   const triggerInput = () => input.click()
 
   const changeHandler = (event) => {
@@ -15,8 +19,23 @@ export function upload(selector, options = {}) {
 
     const files = Array.from(event.target.files)
 
+    preview.innerHTML = ' '
     files.forEach((file) => {
-      console.log(file)
+      if (!file.type.match('image')) return
+
+      const reader = new FileReader()
+
+      reader.onload = (ev) => {
+        const src = ev.target.result
+        input.insertAdjacentHTML(
+          'afterend',
+          `<div class="preview-image">
+            <img src="${src}" alt="${file.name}"/>
+         </div>`
+        )
+      }
+
+      reader.readAsDataURL(file)
     })
   }
 
